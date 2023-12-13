9c3d6eb87d8af05478785fbb7e5f641d98f08134
Dimitris Papagiannis <d.papag.a@gmail.com>
2023-12-13 20:54:01 +0100

    Increase delay between batch jobs launch

--- gpufi_campaign.sh
+++ gpufi_campaign.sh
@@ -377,7 +377,7 @@ batch_execution() {
         # Store the actual cuda executable PID
         _child_pid=$(ps -o pid= --ppid "$_timeout_pid")
         _running_pids+=($_child_pid) # Keep track of background PIDs
-        sleep 1                      # Allow the simulator to properly pick up the config before we modify it.
+        sleep 5                      # Allow the simulator to properly pick up the config before we modify it.
     done
     echo -n "Waiting for batch #$loop_num jobs to complete..."
     wait

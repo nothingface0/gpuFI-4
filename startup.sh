a39d431e3f8458d394247f4314834440f60d79e4
Dimitris Papagiannis <d.papag.a@gmail.com>
2023-12-14 12:03:05 +0100

    Add option to startup

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+env_type=${1:-debug}
 # Source this script in order to setup for gpuFI/GPGPU-Sim execution
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 export PTXAS_CUDA_INSTALL_PATH=/usr/local/cuda-11.0 # For supporting register usage of latest cards
 export CUDA_INSTALL_PATH=/usr/local/cuda-4.2/cuda
 export PATH=$CUDA_INSTALL_PATH/bin:$PATH
-#source setup_environment debug
-source setup_environment
+echo "Setting environment to $env_type"
+source setup_environment $env_type
 printenv >$SCRIPT_DIR/.env # To be used by VSCode debugger
 # Does not work, didn't have time to look into it.
 # echo WYS_EXEC_PATH=$HOME/Documents/workspace/cuda-tests/kernels/test_if/bin/test_if >> $SCRIPT_DIR/.env

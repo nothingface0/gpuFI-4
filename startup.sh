0ce253e5db83d5d268be9836f302881d9952a4a8
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-02-14 18:16:29 +0100

    Cleanup startup

--- startup.sh
+++ startup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# Source this script to run the necessary setup for gpuFI/GPGPU-Sim execution
 env_type=${1:-release}
-# Source this script in order to setup for gpuFI/GPGPU-Sim execution
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 export PTXAS_CUDA_INSTALL_PATH=/usr/local/cuda-11.0 # For supporting register usage of latest cards
 export CUDA_INSTALL_PATH=/usr/local/cuda-4.2/cuda
 export PATH=$CUDA_INSTALL_PATH/bin:$PATH
+if [ "$env_type" != "release" ] && [ "$env_type" != "debug" ]; then
+    echo "Environment type must be \"release\" or \"debug\""
+    return
+fi
 echo "Setting environment to $env_type"
 source setup_environment $env_type
 printenv >$SCRIPT_DIR/.env # To be used by VSCode debugger
-# Does not work, didn't have time to look into it.
-# echo WYS_EXEC_PATH=$HOME/Documents/workspace/cuda-tests/kernels/test_if/bin/test_if >> $SCRIPT_DIR/.env
-# echo WYS_EXEC_NAME=test_if >> $SCRIPT_DIR/.env
 echo PTX_SIM_DEBUG=3 >>$SCRIPT_DIR/.env
+# If c++ std lib debug symbols are needed
 #echo LD_PRELOAD="/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6" >>$SCRIPT_DIR/.env
